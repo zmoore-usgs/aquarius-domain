0a99a5ac1a1620cdcf0bb31d5fa200eac1454073
David M Sibley <dmsibley@usgs.gov>
2015-03-03 16:01:48 -0600

    initial commit

---
+++ src/main/resources/getSchemas.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+XSD_DIRECTORY=target/generated-sources/aqSchema
+
+pwd
+mkdir -p $XSD_DIRECTORY
+
+COUNTER=0
+while [ `curl -s -o /dev/null -w "%{http_code}" "http://nwistssprint.cr.usgs.gov/AQUARIUS/Publish/v2/metadata?xsd=$COUNTER"` -eq 200 ]; do
+	wget "http://nwistssprint.cr.usgs.gov/AQUARIUS/Publish/v2/metadata?xsd=$COUNTER" -O$XSD_DIRECTORY/$COUNTER.xsd
+	let COUNTER=COUNTER+1
+done
+
+sed -i 's/AuthenticateResponse/AuthenticateResponseObj/g' $XSD_DIRECTORY/*.xsd
