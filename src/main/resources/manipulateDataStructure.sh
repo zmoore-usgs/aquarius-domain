4556dc281b86c66970185684b6eebebf52d3b9dc
thongsav-usgs <thongsav@usgs.gov>
2015-03-31 20:25:58 -0500

    DoubleWithDisplay has been implemented and returned as an actual object in responses now.

--- src/main/resources/manipulateDataStructure.sh
+++ src/main/resources/manipulateDataStructure.sh
@@ -8,5 +8,4 @@ sed -i -r 's/type="q[^:]+:ArrayOfNullableOfdouble"/type="xs:double" maxOccurs="u
 sed -i -r 's/type="q([^:]+):ArrayOf([^"]+)"/type="q\1:\2" maxOccurs="unbounded"/g' $XSD_DIRECTORY/*.xsd
 sed -i -r 's/type="q([^:]+):KeyValueOfstringstring" maxOccurs="unbounded"/type="q\1:ArrayOfKeyValueOfstringstring"/g' $XSD_DIRECTORY/*.xsd
 sed -i -r 's/minOccurs="0" name="([^"]+)" nillable="true" type="tns:ArrayOf([^"]+)"/minOccurs="0" name="\1" nillable="true" type="tns:\2" maxOccurs="unbounded"/g' $XSD_DIRECTORY/*.xsd
-sed -i -r 's/minOccurs="0" name="[^"]+" nillable="true" type="tns:DoubleWithDisplay"/minOccurs="0" name="Value" nillable="true" type="xs:double"/g' $XSD_DIRECTORY/*.xsd
 sed -i 's/AuthenticateResponse/AuthenticateResponseObj/g' $XSD_DIRECTORY/*.xsd
