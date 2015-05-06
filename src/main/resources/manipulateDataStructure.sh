1aa6549204816559622c17d2de7a959d237c7422
mike-stern <mwstern@usgs.gov>
2015-05-06 17:03:39 -0500

    make OS X detection work with sed -i

--- src/main/resources/manipulateDataStructure.sh
+++ src/main/resources/manipulateDataStructure.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 XSD_DIRECTORY=target/generated-sources/aqSchema
+bak=""
+if [[ `uname` == "Darwin" ]]; then
+bak=".bak' "
+fi
 
-sed -i -r 's/type="q[^:]+:DateTimeOffset"/type="xs:dateTime"/g' $XSD_DIRECTORY/*.xsd
-sed -i -r 's/type="q[^:]+:ArrayOfstring"/type="xs:string" maxOccurs="unbounded"/g' $XSD_DIRECTORY/*.xsd
-sed -i -r 's/type="q[^:]+:ArrayOfdouble"/type="xs:double" maxOccurs="unbounded"/g' $XSD_DIRECTORY/*.xsd
-sed -i -r 's/type="q[^:]+:ArrayOfNullableOfdouble"/type="xs:double" maxOccurs="unbounded"/g' $XSD_DIRECTORY/*.xsd
-sed -i -r 's/type="q\([^:]+\):ArrayOf\([^"]+\)"/type="q\1:\2" maxOccurs="unbounded"/g' $XSD_DIRECTORY/*.xsd
-sed -i -r 's/type="q\([^:]+\):KeyValueOfstringstring" maxOccurs="unbounded"/type="q\1:ArrayOfKeyValueOfstringstring"/g' $XSD_DIRECTORY/*.xsd
-sed -i -r 's/minOccurs="0" name="\([^"]+\)" nillable="true" type="tns:ArrayOf\([^"]+\)"/minOccurs="0" name="\1" nillable="true" type="tns:\2" maxOccurs="unbounded"/g' $XSD_DIRECTORY/*.xsd
-sed -i 's/AuthenticateResponse/AuthenticateResponseObj/g' $XSD_DIRECTORY/*.xsd
+sed -i -r $bak 's/type="q[^:]+:DateTimeOffset"/type="xs:dateTime"/g' $XSD_DIRECTORY/*.xsd
+sed -i -r $bak 's/type="q[^:]+:ArrayOfstring"/type="xs:string" maxOccurs="unbounded"/g' $XSD_DIRECTORY/*.xsd
+sed -i -r $bak 's/type="q[^:]+:ArrayOfdouble"/type="xs:double" maxOccurs="unbounded"/g' $XSD_DIRECTORY/*.xsd
+sed -i -r $bak 's/type="q[^:]+:ArrayOfNullableOfdouble"/type="xs:double" maxOccurs="unbounded"/g' $XSD_DIRECTORY/*.xsd
+sed -i -r $bak 's/type="q\([^:]+\):ArrayOf\([^"]+\)"/type="q\1:\2" maxOccurs="unbounded"/g' $XSD_DIRECTORY/*.xsd
+sed -i -r $bak 's/type="q\([^:]+\):KeyValueOfstringstring" maxOccurs="unbounded"/type="q\1:ArrayOfKeyValueOfstringstring"/g' $XSD_DIRECTORY/*.xsd
+sed -i -r $bak 's/minOccurs="0" name="\([^"]+\)" nillable="true" type="tns:ArrayOf\([^"]+\)"/minOccurs="0" name="\1" nillable="true" type="tns:\2" maxOccurs="unbounded"/g' $XSD_DIRECTORY/*.xsd
+sed -i $bak 's/AuthenticateResponse/AuthenticateResponseObj/g' $XSD_DIRECTORY/*.xsd
