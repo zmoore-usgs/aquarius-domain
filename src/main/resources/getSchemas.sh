f97336d3f72204bb7f056bd4b0b36c972e1c1ed0
thongsav-usgs <thongsav@usgs.gov>
2015-07-10 10:55:34 -0500

    Get 15.2.135 from integration server

--- src/main/resources/getSchemas.sh
+++ src/main/resources/getSchemas.sh
@@ -5,7 +5,7 @@ pwd
 mkdir -p $XSD_DIRECTORY
 
 COUNTER=0
-while [ `curl -s -o /dev/null -w "%{http_code}" "http://nwistssprint.cr.usgs.gov/AQUARIUS/Publish/v2/metadata?xsd=$COUNTER"` -eq 200 ]; do
-	wget "http://nwistssprint.cr.usgs.gov/AQUARIUS/Publish/v2/metadata?xsd=$COUNTER" -O$XSD_DIRECTORY/$COUNTER.xsd
+while [ `curl -s -o /dev/null -w "%{http_code}" "http://nwists.usgs.gov/AQUARIUS/Publish/v2/metadata?xsd=$COUNTER"` -eq 200 ]; do
+	wget "http://nwists.usgs.gov/AQUARIUS/Publish/v2/metadata?xsd=$COUNTER" -O$XSD_DIRECTORY/$COUNTER.xsd
 	let COUNTER=COUNTER+1
 done
