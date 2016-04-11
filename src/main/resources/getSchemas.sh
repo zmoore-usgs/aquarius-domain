22c90c91e043e3b0bbb6f4523a863d497e5b5c51
thongsav-usgs <thongsav@usgs.gov>
2016-04-11 15:33:54 -0500

    missed a URL

--- src/main/resources/getSchemas.sh
+++ src/main/resources/getSchemas.sh
@@ -6,6 +6,6 @@ mkdir -p $XSD_DIRECTORY
 
 COUNTER=0
 while [ `curl -s -o /dev/null -w "%{http_code}" "http://nwistsmig.cr.usgs.gov/AQUARIUS/Publish/v2/metadata?xsd=$COUNTER"` -eq 200 ]; do
-	wget "http://nwists.usgs.gov/AQUARIUS/Publish/v2/metadata?xsd=$COUNTER" -O$XSD_DIRECTORY/$COUNTER.xsd
+	wget "http://nwistsmig.cr.usgs.gov/AQUARIUS/Publish/v2/metadata?xsd=$COUNTER" -O$XSD_DIRECTORY/$COUNTER.xsd
 	let COUNTER=COUNTER+1
 done
