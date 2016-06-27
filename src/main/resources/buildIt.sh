f3aabb7928dea91d5c697a78534f8713bc323c54
thongsav-usgs <thongsav@usgs.gov>
2016-06-27 10:07:15 -0500

    - Prep for 16.1

--- src/main/resources/buildIt.sh
+++ src/main/resources/buildIt.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+AQ_HOST=$1
+
 pwd
 echo 'START SCHEMA DOWNLOADS'
-src/main/resources/getSchemas.sh
+src/main/resources/getSchemas.sh $AQ_HOST
 echo 'START SCHEMA MANIPULATION'
 src/main/resources/manipulateDataStructure.sh
