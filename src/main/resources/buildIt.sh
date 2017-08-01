36fa4cc53ef867c9a28fa4b59233ed0b418e4bda
zmoore-usgs <zmoore@usgs.gov>
2017-08-01 14:13:22 -0500

    AQCU-1207: Initial changes for Swagger-Codegen

--- src/main/resources/buildIt.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-AQ_HOST=$1
-
-pwd
-echo 'START SCHEMA DOWNLOADS'
-src/main/resources/getSchemas.sh $AQ_HOST
-echo 'START SCHEMA MANIPULATION'
-src/main/resources/manipulateDataStructure.sh
